015d5eaaedcf35cfa0cb286262815f241d52cbd4
kuamillll <blastergmly@gmail.com>
2024-08-28 13:49:03 +0700

    alahhhh

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,136 +1,96 @@
- <style>
-    /* From Uiverse.io by Praashoo7 */ 
-.form {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  padding-left: 2em;
-  padding-right: 2em;
-  padding-bottom: 0.4em;
-  background-color: #171717;
-  border-radius: 25px;
-  transition: .4s ease-in-out;
-}
+ <!DOCTYPE html>
+ <html>
 
-.form:hover {
-  transform: scale(1.05);
-  border: 1px solid black;
-}
+ <head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+   <meta name="author" content="Creative Tim">
+   <title>Argon Dashboard PRO - Premium Bootstrap 4 Admin Template</title>
+   <!-- Favicon -->
+   <link rel="icon" href="../../assets/img/brand/favicon.png" type="image/png">
+   <!-- Fonts -->
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+   <!-- Icons -->
+   <link rel="stylesheet" href="{{ url('assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
+   <link rel="stylesheet" href="{{ url('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
+   <!-- Argon CSS -->
+   <link rel="stylesheet" href="{{ url('assets/css/argon.css') }}" type="text/css">
+ </head>
 
-#heading {
-  text-align: center;
-  margin: 2em;
-  color: rgb(255, 255, 255);
-  font-size: 1.2em;
-}
+ <body class="bg-defaut" style="overflow: hidden;">
+   <div class="main-content">
+     <div class="header bg-gradient-primary py-lg-5 pt-lg-5">
+       <div class="container ">
+         <div class="header-body text-center mb-7">
+           <div class="">
+             <div class="row justify-content-center">
+               <div class="col-lg-5 col-md-7">
+                 <img src="{{ asset('foto_identitas/favpertmin.png')}}" alt="Your Icon" class="mb-3" style="width: 50px; height: 50px;">
+                 <div class="card bg-secondary border-0 mb-0">
+                   <div class="card-body px-lg-5 py-lg-5">
+                     <div class="text-center text-muted mb-4">
+                       <h1>Log in</h1>
+                       <small>Use your Email to Continue</small>
+                     </div>
+                     <form method="POST" action="{{ route('login') }}">
+                       @csrf
+                       <div class="form-group mb-3">
+                         <div class="input-group input-group-merge input-group-alternative">
+                           <div class="input-group-prepend">
+                             <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                           </div>
+                           <input class="form-control" placeholder="Email" name="email" type="email">
+                         </div>
+                       </div>
+                       <div class="form-group mb-2">
+                         <div class="input-group input-group-merge input-group-alternative">
+                           <div class="input-group-prepend">
+                             <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                           </div>
+                           <input class="form-control" placeholder="Password" name="password" type="password">
+                         </div>
+                       </div>
+                       <div class="custom-control custom-control-alternative custom-checkbox">
+                         <input class="custom-control-input" id="remember_me" type="checkbox" name="rememeber">
+                         <label class="custom-control-label d-flex justify-content-start" for="customCheckLogin">
+                           <span class="text-muted">Remember me</span>
+                         </label>
+                       </div>
+                       <div class="text-center ">
+                         <button type="submit" class="btn btn-primary my-4 w-100">Sign in</button>
+                       </div>
+                       <div class="d-flex justify-content-center">
+                         <div class="custom-control custom-control-alternative">
+                           <a href="{{ route('password.request') }}" class="text-primary"><small>Forgot password?</small></a>
+                         </div>
+                       </div>
+                     </form>
+                     <!-- <div class="row mt-3 justify-content-center">
+                       <div class="">
+                         <span class="small">Don't have an account?</span>
+                         <a href="{{ route('register') }}" class="text-primary"><small>Create an account</small></a>
+                       </div>
+                     </div> -->
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
 
-.field {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.5em;
-  border-radius: 25px;
-  padding: 0.6em;
-  border: none;
-  outline: none;
-  color: white;
-  background-color: #171717;
-  box-shadow: inset 2px 5px 10px rgb(5, 5, 5);
-}
+   <script src="{{ url('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+   <script src="{{ url('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+   <script src="{{ url('assets/vendor/js-cookie/js.cookie.js') }}"></script>
+   <script src="{{ url('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+   <script src="{{ url('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
+   <!-- Argon JS -->
+   <script src="{{ url('assets/js/argon.js') }}"></script>
+   <!-- Demo JS - remove this in your project -->
+   <script src="{{ url('assets/js/demo.min.js') }}"></script>
+ </body>
 
-.input-icon {
-  height: 1.3em;
-  width: 1.3em;
-  fill: white;
-}
-
-.input-field {
-  background: none;
-  border: none;
-  outline: none;
-  width: 100%;
-  color: #d3d3d3;
-}
-
-.form .btn {
-  display: flex;
-  justify-content: center;
-  flex-direction: row;
-  margin-top: 2.5em;
-}
-
-.submit {
-  padding: 0.5em;
-  padding-left: 1.1em;
-  padding-right: 1.1em;
-  border-radius: 5px;
-  margin-right: 0.5em;
-  border: none;
-  outline: none;
-  transition: .4s ease-in-out;
-  background-color: #252525;
-  color: white;
-}
-
-.submit:hover {
-  background-color: black;
-  color: white;
-}
-
-.submit {
-  padding: 0.5em;
-  padding-left: 2.3em;
-  padding-right: 2.3em;
-  border-radius: 5px;
-  border: none;
-  outline: none;
-  transition: .4s ease-in-out;
-  background-color: #252525;
-  color: white;
-}
-
-.button2:hover {
-  background-color: black;
-  color: white;
-}
-
-.button3 {
-  margin-bottom: 3em;
-  padding: 0.5em;
-  border-radius: 5px;
-  border: none;
-  outline: none;
-  transition: .4s ease-in-out;
-  background-color: #252525;
-  color: white;
-}
-
-.button3:hover {
-  background-color: red;
-  color: white;
-}
-</style>
-<div class="card">
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-    <p id="heading">Login</p>
-    <div class="field">
-        <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
-    </svg>
-      <input placeholder="Username" name="email" class="input-field" type="text">
-    </div>
-    <div class="field">
-    <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
-    </svg>
-      <input placeholder="Password" class="input-field" name="password" type="password">
-    </div>
-    <div class="btn">
-    <button class="submit">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-    <button class="submit">Sign Up</button>
-</div>
-<button class="button3">Forgot Password</button>
-</form>
-</div>
+ </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,156 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>Argon Dashboard PRO - Premium Bootstrap 4 Admin Template</title>
+
+    <link rel="icon" href="../../assets/img/brand/favicon.png" type="image/png">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+
+    <link rel="stylesheet" href="{{ url('assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ url('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
+
+    <link rel="stylesheet" href="{{ url('assets/css/argon.css') }}" type="text/css">
+</head>
+
+<body class="bg-default" style="overflow: hidden;">
+    <style>
+        .font-weight-700 {
+            font-weight: 700;
+        }
+
+        .text-muted {
+            color: #6c757d;
+        }
+
+        .text-success {
+            color: #28a745;
+        }
+
+        .text-danger {
+            color: #dc3545;
+        }
+
+        .text-warning {
+            color: #ffc107;
+        }
+
+        .text-info {
+            color: #17a2b8;
+        }
+    </style>
+    <div class="main-content">
+        <div class="header bg-gradient-primary py-7 py-lg-4 pt-lg-4">
+            <div class="container">
+                <div class="header-body text-center mb-7">
+                    <div class="">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-6 col-md-8">
+                                <img src="{{ asset('foto_identitas/favpertmin.png')}}" alt="Your Icon" class="mb-3" style="width: 50px; height: 50px;">
+                                <div class="card bg-secondary border-0">
+                                    <div class="card-body px-lg-5 py-lg-5">
+                                        <div class="text-center text-muted mb-1">
+                                            <h1>sign up</h1>
+                                            <span>Create an New account</span>
+                                        </div>
+                                        <form method="POST" action="{{ route('register') }}">
+                                            @csrf
+                                            <div class="form-group">
+                                                <div class="input-group input-group-merge input-group-alternative mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                                                    </div>
+                                                    <input class="form-control" placeholder="username" name="username" type="text" required autofocus autocomplete="username">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="input-group input-group-merge input-group-alternative mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                                    </div>
+                                                    <input class="form-control" placeholder="Email" name="email" type="email" required autocomplete="username">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="input-group input-group-merge input-group-alternative">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="ni ni-lock-circle"></i></span>
+                                                    </div>
+                                                    <input class="form-control" placeholder="Password" id="password" name="password" type="password" required autofocus autocomplete="new-password" onkeyup="checkPasswordStrength(this.value)">
+                                                </div>
+                                                <div class="text-muted font-italic mt-2">
+                                                <small>Password strength: <span id="strengthText"></span></small>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="input-group input-group-merge input-group-alternative">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                                    </div>
+                                                    <input class="form-control" placeholder="password_confirmation" name="password_confirmation" type="password">
+                                                </div>
+                                            </div>
+                                            <div class="text-center">
+                                                <button type="submit" class="btn btn-primary mt-2">Create account</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <script src="{{ url('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/js-cookie/js.cookie.js') }}"></script>
+    <script src="{{ url('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <script src="{{ url('assets/js/argon.js') }}"></script>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+    <script src="{{ url('assets/js/demo.min.js') }}"></script>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <script>
+        function checkPasswordStrength(password) {
+            let strengthText = document.getElementById('strengthText');
+            let strength = 0;
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            if (password.length >= 8) strength += 1;
+            if (password.match(/[a-z]+/)) strength += 1;
+            if (password.match(/[A-Z]+/)) strength += 1;
+            if (password.match(/[0-9]+/)) strength += 1;
+            if (password.match(/[\W]+/)) strength += 1;
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            switch (strength) {
+                case 1:
+                    strengthText.innerHTML = '<span class="text-danger font-weight-700">Very Weak</span>';
+                    break;
+                case 2:
+                    strengthText.innerHTML = '<span class="text-danger font-weight-700">Weak</span>';
+                    break;
+                case 3:
+                    strengthText.innerHTML = '<span class="text-warning font-weight-700">Moderate</span>';
+                    break;
+                case 4:
+                    strengthText.innerHTML = '<span class="text-info font-weight-700">Strong</span>';
+                    break;
+                case 5:
+                    strengthText.innerHTML = '<span class="text-success font-weight-700">Very Strong</span>';
+                    break;
+                default:
+                    strengthText.innerHTML = '';
+            }
+        }
+    </script>
+</body>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
