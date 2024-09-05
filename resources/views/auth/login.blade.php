46bfa7ae6b1ddcfd6144d761ceef1f96a8f16d26
hafifradhitya <bangclasher81@gmail.com>
2024-09-05 10:27:40 +0700

    title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
    <meta name="author" content="Creative Tim">
-   <title>Argon Dashboard PRO - Premium Bootstrap 4 Admin Template</title>
+   <title>Masuk untuk akses</title>
    <!-- Favicon -->
    <link rel="icon" href="../../assets/img/brand/favicon.png" type="image/png">
    <!-- Fonts -->
